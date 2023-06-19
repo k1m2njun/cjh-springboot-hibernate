37af038961a610032a308b9dbaceb2755b28444b
k1m2njun <k1m2njun@naver.com>
2023-06-19 19:34:41 +0900

    init

---
+++ src/main/java/com/example/springboothiber/SpringbootHiberApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboothiber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHiberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHiberApplication.class, args);
+    }
+
+}
